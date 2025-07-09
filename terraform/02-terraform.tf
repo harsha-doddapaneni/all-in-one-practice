8a2483af15555a79212bca394429484320a765fb
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-09 08:20:21 +0530

    terraform

--- terraform/02-terraform.tf
+++ terraform/02-terraform.tf
@@ -13,10 +13,10 @@ resource "aws_security_group" "allow_ssh_terraform" {
     description= var.sg_description
 
     egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port = var.from_port
+        to_port = var.to_port
+        protocol = var.protocol
+        cidr_blocks = var.ingress_cidr
         ipv6_cidr_blocks = ["::/0"]
     }
 
