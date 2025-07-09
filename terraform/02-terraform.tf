2448941cffa83f1b99cccbe0a33f01f76583b23d
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-09 08:06:24 +0530

    terraform

--- terraform/02-terraform.tf
+++ terraform/02-terraform.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "terraform" {
+    
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
