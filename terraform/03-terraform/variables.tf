ce8eac3fd1573864679dcdf76dbb501d3a40cd90
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-09 09:58:40 +0530

    terraform

--- terraform/03-terraform/variables.tf
+++ terraform/03-terraform/variables.tf
@@ -55,3 +55,11 @@ variable "instance_names" {
     type = list(string)
     default = ["mysql", "backend", "frontend"]
 }
+
+variable "zone_id" {
+    default = "Z0486123205W1JWLD2711"
+}
+
+variable "domain_name" {
+    default = "doddapaneni.shop"
+}
