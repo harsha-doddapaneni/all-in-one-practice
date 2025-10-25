fadf971ae44a70d2193096fa89eca11ea989b90c
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-10-25 08:40:24 +0530

    git

--- terraform/01-terraform/terraform.tf
+++ terraform/01-terraform/terraform.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
-    
+
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
@@ -9,8 +9,8 @@ resource "aws_instance" "terraform" {
 }
 
 resource "aws_security_group" "allow_ssh_terraform" {
-    name= "allow_ssh"
-    description= "allow port no 22 for ssh access"
+    name = "allow_ssh"
+    description = "allow port no 22 for ssh access"
 
     egress {
         from_port = 0
@@ -19,7 +19,6 @@ resource "aws_security_group" "allow_ssh_terraform" {
         cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
-
     ingress {
         from_port = 22
         to_port = 22
@@ -27,6 +26,7 @@ resource "aws_security_group" "allow_ssh_terraform" {
         cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
+
     tags = {
         Name = "allow_ssh"
     }
