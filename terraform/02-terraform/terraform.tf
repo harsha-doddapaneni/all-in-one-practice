d5631a476d8f5ff803eec0fa91bd53b6a23d2a0d
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-10-25 09:07:04 +0530

    git

--- terraform/02-terraform/terraform.tf
+++ terraform/02-terraform/terraform.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terraform" {
 
     ami = var.ami_id
-    instance_type = var.environment == "prod" ? "t3.small" : "t3.micro"
+    instance_type = var.environment == "dev" ? "t3.small" : "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = var.tags
 }
